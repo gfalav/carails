ee523b0cc4afb551eed3098f6dca2f279776c6f8
gfalavigna <gfalavigna@ubuntu.ubuntu-domain>
2011-05-09 13:49:18 +0200

    arreglados prob de deploy

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Carails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+    root :to => "tasks#index"
 
   # See how all your routes lay out with "rake routes"
 
